9f93501e7cbf544d1c5674331a3471fa86f2029e
nagahama_windows <nagaha1223@gmail.com>
2020-10-17 13:26:00 +0900

     

---
+++ index.js
@@ -0,0 +1,12 @@
+const test = function(name){
+    console.log("こんにちは");
+}
+
+test();
+
+const introduce = (sta) => {
+    console.log('もじれつ結合'+ sta + 'やっぱりこうだよね');
+    //console.log(sta);
+}
+
+introduce("そうｓだ");
