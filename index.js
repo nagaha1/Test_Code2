255eaf479b36dad263edae7abba4103260d345e3
ナガハマ・ナガハマ <nagahamanagahama@nagahama-MacBook-Pro.local>
2020-10-18 14:17:44 +0900

    20201018

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
-const test = function(name){
-    console.log("こんにちは");
-}
+const test = function(name) {
+  console.log("こんにちは");
+};
 
 test();
 
-const introduce = (sta) => {
-    console.log('もじれつ結合'+ sta + 'やっぱりこうだよね');
-    //console.log(sta);
-}
+const introduce = sta => {
+  console.log("もじれつ結合" + sta + "やっぱりこうだよね");
+  //console.log(sta);
+};
 
 introduce("そうｓだ");
+
+class ani {
+  // const an = function an_func(params) {
+  //     console.log('functionはこうつくるの');
+  // }
+
+  constructor() {
+    this.name = console.log("eeeeee");
+  }
+
+  gree = () => {
+    console.log("サウンドが良い");
+  };
+}
+
+//うごきが面白いな、そうなるのかname変数にconsole.logが入っているのインスタが作成されて段階でそうなるのか。
+
+const anima = new ani();
+//anima.gree();
+//console.log(anima.name);
+
+// 完全数かどうか
+
+/*
+ は完全数です。
+実際，6 の約数は 1,2,3,6 で，全て足すと
+1+2+3+6=12 となり，6 の2倍になっています。
+496 も完全数です。
+
+２８は完全数なのか
+*/
+
+var p_nume = 28;
+
+p_nume *= 2;
+
+console.log(p_nume);
+// 数字に関しての脳死ぐいか
